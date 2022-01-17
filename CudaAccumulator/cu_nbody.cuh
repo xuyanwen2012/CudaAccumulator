8623b1d075a48f0f5cdc30b3b962079fb2d6ce6a
Ivan Xu <info@chivalrystudio.com>
2022-01-17 10:11:51 -0800

    Add project files.

---
+++ CudaAccumulator/cu_nbody.cuh
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <cuda_runtime_api.h>
+
+static void handle_error(const cudaError_t err,
+                         const char* file,
+                         const int line)
+{
+	if (err != cudaSuccess)
+	{
+		printf("%s in %s at line %d\n", cudaGetErrorString(err),
+		       file, line);
+		exit(EXIT_FAILURE);
+	}
+}
+
+#define HANDLE_ERROR( err ) (handle_error( err, __FILE__, __LINE__ ))
+
+void compute_with_cuda();
---
+++ CudaAccumulator/main.cpp
@@ -0,0 +1,6 @@
+#include "cu_nbody.cuh"
+
+int main(int argc, char* argv[])
+{
+	compute_with_cuda();
+}
