3cc424709c20d56d699f27bed7fc83adcd8f912f
Ivan Xu <info@chivalrystudio.com>
2022-01-24 11:03:52 -0800

    save

--- CudaAccumulator/main.cpp
+++ CudaAccumulator/main.cpp
@@ -23,6 +23,38 @@ float my_rand(const float f_min = 0.0, const float f_max = 1.0)
 #endif
 
 
+void print_ground_truth(const float* xs, const float* ys, const float* masses, const unsigned n)
+{
+	constexpr unsigned n_to_print = 10;
+	std::array<std::pair<float, float>, n_to_print> us{};
+
+	for (unsigned i = 0; i < n_to_print; ++i)
+	{
+		us[i] = {0.0f, 0.0f};
+		for (unsigned j = 0; j < n; ++j)
+		{
+			const float dx = xs[i] - xs[j];
+			const float dy = ys[i] - ys[j];
+
+			const float dist_sqr = dx * dx + dy * dy + 1e-9f;
+			const float inv_dist = 1.0f / sqrtf(dist_sqr);
+			const float inv_dist3 = inv_dist * inv_dist * inv_dist;
+			const float with_mass = inv_dist3 * masses[j]; // z is the mass in this case
+
+			us[i].first += dx * with_mass;
+			us[i].second += dy * with_mass;
+		}
+	}
+
+	std::cout << "==================" << std::endl;
+	for (unsigned i = 0; i < n_to_print; ++i)
+	{
+		std::cout << '('
+			<< us[i].first << ", "
+			<< us[i].second << ')' << std::endl;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	constexpr int num_bodies = 1024;
@@ -62,6 +94,8 @@ int main(int argc, char* argv[])
 	{
 		std::cout << '(' << us[i].x << ", " << us[i].y << ')' << std::endl;
 	}
+
+	print_ground_truth(xs.data(), ys.data(), masses.data(), num_bodies);
 
 	return EXIT_SUCCESS;
 }
