3a871c98da05d50ea07167dd99ebf2cc1a6b2f0c
Ivan Xu <info@chivalrystudio.com>
2022-01-27 20:21:00 -0800

    save

--- CudaAccumulator/accumulator.cu
+++ CudaAccumulator/accumulator.cu
@@ -22,10 +22,9 @@ using accumulator_handle = struct accumulator_handle
 	float y;
 
 	// CUDA Related
-	std::vector<float3> bodies_buf;
-
-	float3* dev_bodies; // stores the {x, y, mass} for all particles i..n
-	float2* dev_result; // stores the result of reduced force at [0]
+	unsigned int body_count;
+	float3* uni_bodies;
+	float2* uni_results;
 };
 
 
@@ -101,25 +100,17 @@ __global__ void force_reduction_2(const float3 source_body, const float3* bodies
 }
 
 
-std::array<float2, 1> compute_with_cuda(const accumulator_handle* acc, const unsigned n)
+float2 compute_with_cuda(const accumulator_handle* acc, const unsigned array_start, const unsigned n)
 {
-	// printf("	debug: shipped %d to GPU \n", n);
-
-	const unsigned bytes_f3 = n * sizeof(float3);
-
-	HANDLE_ERROR(cudaMemcpy(acc->dev_bodies, acc->bodies_buf.data(), bytes_f3, cudaMemcpyHostToDevice));
-
 	const unsigned block_size = n;
 	constexpr unsigned grid_size = 1;
-
 	const auto source_body = make_float3(acc->x, acc->y, 1.0f);
 
-	force_reduction_2 << <grid_size, block_size >> >(source_body, acc->dev_bodies, acc->dev_result, n);
+	force_reduction_2 << <grid_size, block_size >> >(source_body, acc->uni_bodies + array_start, acc->uni_results, n);
 
-	std::array<float2, 1> result{};
-	HANDLE_ERROR(cudaMemcpy(result.data(), acc->dev_result, sizeof(float2), cudaMemcpyDeviceToHost));
+	cudaDeviceSynchronize();
 
-	return result;
+	return acc->uni_results[0];
 }
 
 
@@ -132,10 +123,10 @@ accumulator_handle* get_accumulator()
 	constexpr unsigned bytes_f3 = max_num_bodies_per_compute * sizeof(float3);
 	constexpr unsigned bytes_f2 = max_num_bodies_per_compute * sizeof(float3);
 
-	HANDLE_ERROR(cudaMalloc(reinterpret_cast<void**>(&acc->dev_bodies), bytes_f3));
-	HANDLE_ERROR(cudaMalloc(reinterpret_cast<void**>(&acc->dev_result), bytes_f2));
+	HANDLE_ERROR(cudaMallocManaged(reinterpret_cast<void**>(&acc->uni_bodies), bytes_f3));
+	HANDLE_ERROR(cudaMallocManaged(reinterpret_cast<void**>(&acc->uni_results), bytes_f2));
 
-	acc->bodies_buf = {};
+	acc->body_count = 0;
 
 	return acc;
 }
@@ -144,33 +135,33 @@ accumulator_handle* get_accumulator()
 int check_and_clear_current_buffer(accumulator_handle* acc)
 {
 	// Finished the remaining bodies in the buffer before switching context
-	if (!acc->bodies_buf.empty())
+	if (acc->body_count != 0)
 	{
-		auto remaining_n = static_cast<uint32_t>(acc->bodies_buf.size());
+		auto remaining_n = acc->body_count;
 
 		float2 rem_force = {};
 
+		unsigned current_index = 0;
 		while (remaining_n >= 32)
 		{
 			const uint32_t previous_pow_of_2 = flp2(remaining_n);
 
-			const auto result = compute_with_cuda(acc, previous_pow_of_2);
-			rem_force.x += result[0].x;
-			rem_force.y += result[0].y;
+			const auto result = compute_with_cuda(acc, current_index, previous_pow_of_2);
+			rem_force.x += result.x;
+			rem_force.y += result.y;
 
 			remaining_n -= previous_pow_of_2;
 
 			// drop the first 'previous_pow_of_2' particles
-			std::vector<decltype(acc->bodies_buf)::value_type>(acc->bodies_buf.begin() + previous_pow_of_2,
-			                                                   acc->bodies_buf.end()).swap(acc->bodies_buf);
+			current_index += previous_pow_of_2;
 		}
 
 		// Do the rest on CPU ( < 32)
-		for (unsigned j = 0; j < remaining_n; ++j)
+		for (unsigned j = current_index; j < current_index + remaining_n; ++j)
 		{
-			const auto dx = acc->x - acc->bodies_buf[j].x;
-			const auto dy = acc->y - acc->bodies_buf[j].y;
-			const auto mass = acc->bodies_buf[j].z;
+			const auto dx = acc->x - acc->uni_bodies[j].x;
+			const auto dy = acc->y - acc->uni_bodies[j].y;
+			const auto mass = acc->uni_bodies[j].z;
 
 			const auto result = kernel_func_cpu(dx, dy, mass);
 			rem_force.x += result.x;
@@ -180,11 +171,6 @@ int check_and_clear_current_buffer(accumulator_handle* acc)
 		float* tmp = acc->result_addr;
 		tmp[0] += rem_force.x;
 		tmp[1] += rem_force.y;
-
-		//if (remaining_n > 0)
-		//{
-		//	printf("	debug: %d was done on CPU, force is now %f,%f \n", remaining_n, rem_force.x, rem_force.y);
-		//}
 	}
 
 	return 0;
@@ -196,7 +182,7 @@ int accumulator_set_constants_and_result_address(const float x, const float y, f
 	acc->x = x;
 	acc->y = y;
 	acc->result_addr = addr;
-	acc->bodies_buf.clear();
+	acc->body_count = 0;
 
 	return 0;
 }
@@ -204,8 +190,8 @@ int accumulator_set_constants_and_result_address(const float x, const float y, f
 
 int release_accumulator(const accumulator_handle* acc)
 {
-	cudaFree(acc->dev_bodies);
-	cudaFree(acc->dev_result);
+	cudaFree(acc->uni_bodies);
+	cudaFree(acc->uni_results);
 
 	HANDLE_ERROR(cudaDeviceReset());
 	return 0;
@@ -215,18 +201,19 @@ int release_accumulator(const accumulator_handle* acc)
 int accumulator_accumulate(const float x, const float y, const float mass, accumulator_handle* acc)
 {
 	// Push this to the buffer 
-	acc->bodies_buf.push_back(make_float3(x, y, mass));
+	acc->uni_bodies[acc->body_count] = make_float3(x, y, mass);
+	++acc->body_count;
 
-	if (acc->bodies_buf.size() >= max_num_bodies_per_compute)
+	if (acc->body_count >= max_num_bodies_per_compute)
 	{
-		const auto result = compute_with_cuda(acc, max_num_bodies_per_compute);
+		const auto result = compute_with_cuda(acc, 0, max_num_bodies_per_compute);
 
 		// Storing the result back 
 		float* tmp = acc->result_addr;
-		tmp[0] += result[0].x;
-		tmp[1] += result[0].y;
+		tmp[0] += result.x;
+		tmp[1] += result.y;
 
-		acc->bodies_buf.clear();
+		acc->body_count = 0;
 	}
 
 	return 0;
--- CudaAccumulator/util.h
+++ CudaAccumulator/util.h
@@ -5,13 +5,6 @@
 
 #include "body.h"
 
-//#define TIME_THIS_SEGMENT(lines)  \
-//const auto start = std::chrono::steady_clock::now();\
-//lines;\
-//const auto end = std::chrono::steady_clock::now();\
-//const std::chrono::duration<double> elapsed_seconds = end - start;\
-//std::cout << "- elapsed time: " << elapsed_seconds.count() << "s\n";
-
 float my_rand();
 
 void print_ground_truth(const std::vector<std::shared_ptr<body<float>>>& bodies);
