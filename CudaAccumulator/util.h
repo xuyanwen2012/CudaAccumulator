fe5750a282a3b80c6401ea66aa81873a9da55fc5
Ivan Xu <info@chivalrystudio.com>
2022-01-30 21:18:24 -0800

    save

--- CudaAccumulator/util.h
+++ CudaAccumulator/util.h
@@ -3,6 +3,6 @@
 #include <type_traits>
 
 template <class Ty>
-_INLINE_VAR constexpr bool my_is_floating_point_v = std::_Is_any_of_v<std::remove_cv_t<Ty>, float, double, long double>;
+constexpr bool my_is_floating_point_v = std::_Is_any_of_v<std::remove_cv_t<Ty>, float, double, long double>;
 
 float my_rand();
