607eafee0dce8ddbcef9a5d759763fc6b4a59c6e
Ivan Xu <info@chivalrystudio.com>
2022-02-02 13:22:54 -0800

    kd_tree

---
+++ CudaAccumulator/kd_tree.cpp
@@ -0,0 +1,35 @@
+﻿#include "kd_tree.h"
+
+node::node(const float x, const float y) : point{x, y}, left(), right()
+{
+}
+
+kd_tree::kd_tree() : root_()
+{
+}
+
+node* kd_tree::insert_point(const point_t& point)
+{
+	return inner_insert_point(root_, point, 0);
+}
+
+node* kd_tree::inner_insert_point(node* current, const point_t& point, const unsigned depth)
+{
+	if (current == nullptr)
+	{
+		return new node(point[0], point[1]);
+	}
+
+	// cd == 0 is x-axis, 1 is y-axis.
+	const unsigned cd = depth % k;
+	if (point[cd] < current->point[cd])
+	{
+		current->left = inner_insert_point(current->left, point, depth + 1);
+	}
+	else
+	{
+		current->right = inner_insert_point(current->right, point, depth + 1);
+	}
+
+	return current;
+}
---
+++ CudaAccumulator/kd_tree.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+constexpr unsigned k = 2;
+using point_t = float[k];
+
+// A structure to represent node of kd tree
+struct node
+{
+	node() = default;
+	node(float x, float y);
+	point_t point;
+
+	node* left;
+	node* right;
+};
+
+class kd_tree
+{
+public:
+	kd_tree();
+
+	node* insert_point(const point_t& point);
+
+private:
+	static node* inner_insert_point(node* current, const point_t& point, unsigned depth);
+
+	node* root_;
+};
